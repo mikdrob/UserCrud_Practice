dff40123b160ad7897d652b9b295194579aab628
midrob <midrob@ttu.ee>
2021-07-29 17:00:36 +0300

    security

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -28,6 +28,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/api/**").hasRole(ADMIN.name())
                 .anyRequest()
